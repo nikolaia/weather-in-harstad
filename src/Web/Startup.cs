6904c10f36beab51918bb9747f539b40ff2a67b8
nikolaia <nikolaia@users.noreply.github.com>
2017-08-22 17:25:28 +0200

    Add ef core

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,34 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyLib;
 
 namespace Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFrameworkNpgsql().AddDbContext<BloggingContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, BloggingContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Error");
+            }
+
+            var blog = new Blog
+            {
+                Url = "http://blogs.msdn.com/dotnet",
+                Posts = new List<Post>
+                {
+                    new Post { Title = "Intro to C#" },
+                    new Post { Title = "Intro to VB.NET" },
+                    new Post { Title = "Intro to F#" }
+                }
+            };
+
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
         }
     }
 }
