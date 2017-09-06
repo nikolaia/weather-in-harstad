0e6c04997da61215892d614c6a4fd52e6556811b
nikolaia <nikolaia@users.noreply.github.com>
2017-09-06 18:10:02 +0200

    Add infrastructure and slides

---
+++ infrastructure/azure_webapp.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# Variables
+resourceGroupName="miniseminaret2017"
+servicePlanName="AppServicePlan"
+appName="ms17app"
+location="westeurope"
+
+# Create a Resource Group
+az group create --name $resourceGroupName --location $location
+
+# Create an App Service Plan
+az appservice plan create --name $servicePlanName --resource-group $resourceGroupName --location $location --is-linux --sku S1
+
+# Create a Web App
+az webapp create --name $appName --plan $servicePlanName --resource-group $resourceGroupName --is-linux
+
+# Create local-git source-control for the Web App
+az webapp deployment source config-local-git --name $appName --resource-group $resourceGroupName
+
+# Create a Postgres Server
+az postgres server create -l $location -g $resourceGroupName -n miniseminaret2017 -u miniseminaret2017 -p "8be7AzZuty*R9zdQMuPX2jfs&%Y22Z"
+
+# git remote add azure https://@MyUniqueApp.scm.azurewebsites.net/MyUniqueApp.git
+
+# Configure Web App with a Custom Docker Container from Docker Hub
+# az webapp config container set --docker-custom-image-name $dockerHubContainerPath --name $appName --resource-group myResourceGroup
+
+# Clean up deployment
+# az group delete --name $resourceGroupName
---
+++ slides/slides/index.md
@@ -0,0 +1,12 @@
+- title : En backend for hobbyprosjekter på rekordtid
+- description : Finnes ikke noe kjipere enn når man har en god idé, men ikke helt vet hvor man skal starte. Man trenger jo å få satt opp noe i skyen, noe som kan lagre i en database og helst skal det logge ting slik at vi vet hva som skjer. Det må også være enkelt å putte ut en ny versjon av appen. Mye greier, men vi skal vell sannelig klare å komme oss fra “File -> New” til kjørende produkt på under halvtimen 💪.
+- author : Nikolai Norman Andersen
+- theme : night
+- transition : default
+
+***
+
+# En backend for hobbyprosjekter
+##  på rekordtid
+
+***
