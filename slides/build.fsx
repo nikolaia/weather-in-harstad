7c7d2051f7547cbdadda325b0d07e5882c60630d
nikolaia <nikolaia@users.noreply.github.com>
2017-09-08 09:59:24 +0200

    Update build.fsx in slides/

--- slides/build.fsx
+++ slides/build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
-let gitHome = "https://github.com/" + gitOwner
+let gitOwner = "nikolaia"
+let gitHome = "git@github.com:" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "miniseminar2017-dotnet"
 
 open FsReveal
 open Fake
