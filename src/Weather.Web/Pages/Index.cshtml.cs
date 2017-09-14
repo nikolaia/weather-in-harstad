dfcad0377f85679b0a0abff07a6c83d8870c64ff
nikolaia <nikolaia@users.noreply.github.com>
2017-09-14 15:34:17 +0200

    Add logevents

--- src/Weather.Web/Pages/Index.cshtml.cs
+++ src/Weather.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using Weather.Models;
 
 namespace Weather.Web.Pages
@@ -12,12 +14,24 @@ namespace Weather.Web.Pages
 
         public async Task OnGet()
         {
+            Log.Information("Someone is attempting to fetch the weather for Harstad");
+            
+            var logtemplate = "{provider} is {temperature} degrees";
+            
+            var yr = await Weather.Integrations.Yr.GetForecastHarstad();
+            Log.Information(logtemplate, yr.Provider, yr.TempString);
+            
+            var storm = await Weather.Integrations.Storm.GetForecast("Harstad");
+            Log.Information(logtemplate, storm.Provider, storm.TempString);
+            
             Temperatures = new List<View.TempViewModel>
             {
-                View.TempViewModel.Create(await Weather.Integrations.Yr.GetForecastHarstad()),
-                View.TempViewModel.Create(await Weather.Integrations.Storm.GetForecast("Harstad")),
+                View.TempViewModel.Create(yr),
+                View.TempViewModel.Create(storm),
                 new View.TempViewModel("Bestefar", "Trist")
             };
+            
+            Log.Information("Temperatures are @{Temperatures}", Temperatures);
         }
     }
 }
