88b46d473c9a7b6643639c8452589e92c01e3f55
nikolaia <nikolaia@users.noreply.github.com>
2018-05-15 15:31:21 +0200

    Add comments in build.fsx

--- build.fsx
+++ build.fsx
@@ -1,37 +1,46 @@
+// Information specific for this solution
 let appName = "Weather"
 let sln = sprintf "%s.sln" appName
 
+// Dependencies and FAKE-specific stuff
+// ==============================================
 #r "paket:nuget Fake.Core.Target //"
 #r "paket:nuget Fake.DotNet.Cli //"
 #r "paket:nuget Fake.IO.FileSystem //"
 #r "paket:nuget Fake.IO.Zip //"
-
-// ==============================================
-// Use this for IDE support. Not required by FAKE 5. Change "build.fsx" to the name of your script.
+// Use this for IDE support. Not required by FAKE 5.
 #load ".fake/build.fsx/intellisense.fsx"
-
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+// ==============================================
 
+// We fetch the version from the current buildServer if possible (TeamCity, VSTS etc.)
 let version = 
         match BuildServer.buildServer with 
         | TeamFoundation -> BuildServer.buildVersion
         | _ -> "1.0.0-beta4"
 
+// Ensure that the 'build' and 'artifacts' directory exists
 let buildDir = __SOURCE_DIRECTORY__ + "/build"
 Directory.ensure buildDir
 let artifactDir = __SOURCE_DIRECTORY__ + "/artifacts"
 Directory.ensure artifactDir
+
+// Clean the 'build' and 'artifacts' folder to prepare for a new build
 Target.create "Clean" <| fun _ ->
     Shell.cleanDirs [buildDir; artifactDir;]
 
+// Use the given dotnet SDK to restore packages and build the solution into the 'build' folder
 Target.create "Build" <| fun _ ->
     let install = lazy DotNet.install DotNet.Release_2_1_4
     let inline dotnetSimple arg = DotNet.Options.lift install.Value arg 
-    DotNet.build (fun opt -> { opt with OutputPath = Some buildDir } |> dotnetSimple) "Weather.sln" 
-    
+    DotNet.build (fun opt -> { opt with OutputPath = Some buildDir } |> dotnetSimple) sln
+
+// Zip the 'build' folder and place the zip in the 'artifacts' folder together with the
+// ARM-templates and the upload scripts.
 Target.create "Artifact" <| fun _ ->
     let artifactFilename = sprintf "%s/%s.%s.zip" artifactDir appName version
 
@@ -44,8 +53,6 @@ Target.create "Artifact" <| fun _ ->
 
     Shell.copyFile artifactDir "upload.cmd"
     Shell.copyFile artifactDir "upload.ps1"
-
-open Fake.Core.TargetOperators
 
 "Clean"
     ==> "Build"
