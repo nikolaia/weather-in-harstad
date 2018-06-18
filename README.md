2906fd00da00ef75767d583c08c24bc0642d6d71
Nikolai Norman Andersen <nikolai@stasj.com>
2018-06-18 07:20:49 +0200

    Use FAKE 5.1.0. Remove workaround in deploy.fsx that was fixed with bugfix in 5.1.0

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Install .NET Core 2.1 SDK (v2.1.300) or higher. This is the version that introdu
 Install Azure CLI 2.0+, run `az login` and make sure you are on the correct subscription, then:
 
 ```powershell
-dotnet tool install fake-cli -g --version 5.0.0*
+dotnet tool install fake-cli -g --version 5.1.0*
 fake run build.fsx
 fake run provision.fsx appName=yourWebAppName parameterSet=test
 ```
@@ -20,7 +20,7 @@ The `deploy.fsx` deployment script is made to be run from inside of Kudu and is 
 
 ```ini
 [config]
-command = dotnet tool install fake-cli -g --version 5.0.0* & D:\local\UserProfile\.dotnet\tools\fake run deploy.fsx stageFolder=app/
+command = dotnet tool install fake-cli -g --version 5.1.0* & D:\local\UserProfile\.dotnet\tools\fake run deploy.fsx stageFolder=app/
 ```
 
 ## Global tools on KUDU
@@ -28,8 +28,8 @@ command = dotnet tool install fake-cli -g --version 5.0.0* & D:\local\UserProfil
 Global tools are not in PATH by default inside the kudu sandbox:
 
 ```powershell
-PS D:\home\site\wwwroot> dotnet tool install fake-cli -g --version 5.0.0*
-dotnet tool install fake-cli -g --version 5.0.0*
+PS D:\home\site\wwwroot> dotnet tool install fake-cli -g --version 5.1.0*
+dotnet tool install fake-cli -g --version 5.1.0*
 Tools directory 'D:\local\UserProfile\.dotnet\tools' is not currently on the PATH environment variable.
 
 You can add the directory to the PATH by running the following command:
@@ -37,7 +37,7 @@ You can add the directory to the PATH by running the following command:
 setx PATH "%PATH%;D:\local\UserProfile\.dotnet\tools"
 
 You can invoke the tool using the following command: fake
-Tool 'fake-cli' (version '5.0.0') was successfully installed.
+Tool 'fake-cli' (version '5.1.0') was successfully installed.
 ```
 
 We are not allowed to run the suggested command, but since the UserProfile folder is predictable we can simply run it from the hardcoded path inside of the `.deployment`-file.
--- deploy.fsx
+++ deploy.fsx
@@ -7,30 +7,6 @@ nuget Fake.Azure.Kudu //"
 open Fake.Core
 open Fake.Azure
 open Fake.Core.TargetOperators
-open Fake.IO
-
-// All of this KuduSync stuff is to get around some bugs until it's fixed in the fake module: https://github.com/fsharp/FAKE/pull/1995 
-let kuduPath =
-    Process.tryFindFileOnPath "kudusync"
-    |> function
-        | Some kudusync when File.Exists kudusync -> kudusync |> Path.GetDirectoryName |> DirectoryInfo.ofPath
-        | _ -> failwith "Couldn't find az command on PATH" 
-
-/// Synchronises all staged files from the temporary deployment to the actual deployment, removing
-/// any obsolete files, updating changed files and adding new files.
-let kuduSync() =
-    let result =
-        Process.execWithResult(fun psi ->
-            { psi with
-                FileName = Path.Combine(kuduPath.FullName, "kudusync.cmd")
-                Arguments = sprintf """-v 50 -f "%s" -t "%s" -n "%s" -p "%s" -i ".git;.hg;.deployment;deploy.cmd" """ 
-                                Kudu.deploymentTemp 
-                                Kudu.deploymentTarget 
-                                Kudu.nextManifestPath
-                                Kudu.previousManifestPath })
-            (System.TimeSpan.FromMinutes 5.)
-    result.Results |> Seq.iter (fun cm -> printfn "%O: %s" cm.Timestamp cm.Message)
-    if not result.OK then failwith "Error occurred during Kudu Sync deployment."
     
 Target.create "KuduStageFiles" <| fun tp ->
     tp.Context.Arguments
@@ -41,7 +17,7 @@ Target.create "KuduStageFiles" <| fun tp ->
         | Some app -> Kudu.stageFolder app (fun _ -> true) // TODO: use build/app locally and /app on kudu :thinking_face:
         | None -> failwith "Please provide a folder to stage as the only argument of the script (fake run ./deploy.fsx stageFolder=/app)"
 
-Target.create "Deploy" <| fun _ ->  kuduSync()
+Target.create "Deploy" <| fun _ ->  Kudu.kuduSync()
 
 "KuduStageFiles"
     ==> "Deploy"
